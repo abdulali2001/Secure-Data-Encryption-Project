82f14c83cb97ea6738be9e5a92a215f400fceedf
Abdul Ali <164244793+abdulali2001@users.noreply.github.com>
2025-04-19 15:04:30 +0500

    Update datasecure.py

--- datasecure.py
+++ datasecure.py
@@ -64,11 +64,13 @@ choice = st.sidebar.selectbox("Navigation", menu)
 
 if choice =="Home":
     st.subheader("Welcome To My 🔐 Data Encryption System Using Streamlit !")
-    st.markdown(Develop a Streamlit-based secure data storage and retrieval system where:Users store data with a unique passkey.
-    Users decrypt data by providing the correct passkey.
-    Multiple failed attempts result in a forced reauthorization (login page).
-    The system operates entirely in memory without external databases.)
-
+    st.markdown("Develop a Streamlit-based secure data storage and retrieval system where: Users store data with a unique passkey.")
+    st.markdown("""
+  Develop a Streamlit-based secure data storage and retrieval system where:
+ - Users store data with a unique passkey.
+ - Retrieval requires the correct passkey.
+ - Data is encrypted during storage.
+ """)
 # === user registration ===
 elif choice == "Register":
     st.subheader("✏️ Register New User")
